ba11cda3d7279ece8b7c2e0fd523a38a82f4a128
s4ndhyac <sandhya2392@gmail.com>
2019-04-26 19:52:50 -0700

    renaming files

--- main-pipe238p.c
+++ main-pipe238p.c
@@ -10,46 +10,38 @@
 
 int main(void)
 {
-  pid_t c1;
-  pid_t c2;
-  int pipe1[2], pipe2[2];
-  if (pipe(pipe2) < 0)
-  {
-    perror("Error in pipe1");
-  }
-  c1 = fork();
-  if (c1 == 0)
-  {
-    //first child
+  pid_t child_1;
+  pid_t child_2;
+  int pipe_1_fds[2];
+  int pipe_2_fds[2];
 
-    if (pipe(pipe1) < 0)
+  if (pipe(pipe_2_fds) < 0)
+    fprintf(stderr, "Error in pipe 2 system call\n");
+
+  if (fork() == 0)
+  {
+    if (pipe(pipe_1_fds) < 0)
+      fprintf(stderr, "Error in pipe 1 system call\n");
+    if (fork() == 0)
     {
-      perror("Error in pipe2");
-    }
-    c2 = fork();
-    if (c2 == 0)
-    {
-      //second child
-      dup2(pipe1[1], 1);
-      close(pipe1[0]);
+      dup2(pipe_1_fds[1], 1);
+      close(pipe_1_fds[0]);
       execl("/bin/ls", "ls", NULL);
       exit(0);
     }
 
-    //first child
-    dup2(pipe1[0], 0);
-    close(pipe1[1]);
-    dup2(pipe2[1], 1);
+    dup2(pipe_1_fds[0], 0);
+    close(pipe_1_fds[1]);
+    dup2(pipe_2_fds[1], 1);
     execl("/bin/grep", "grep", "main", NULL);
 
     exit(0);
   }
   else
   {
-    dup2(pipe2[0], 0);
-    close(pipe2[1]);
+    dup2(pipe_2_fds[0], 0);
+    close(pipe_2_fds[1]);
     execlp("/usr/bin/wc", "wc", NULL);
-    //parent
     exit(0);
   }
   exit(0);
