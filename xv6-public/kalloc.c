5d593cca22922ef27dfdb3c643de24a4e6eb2211
s4ndhyac <sandhya2392@gmail.com>
2019-05-27 17:09:31 -0700

    adding definition

--- xv6-public/kalloc.c
+++ xv6-public/kalloc.c
@@ -10,6 +10,7 @@
 #include "spinlock.h"
 
 void freerange(void *vstart, void *vend);
+int kcount(void);
 extern char end[]; // first address after kernel loaded from ELF file
                    // defined by the kernel linker script in kernel.ld
 
