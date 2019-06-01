e9303186a4c6dc249a009120fca8d1f712310a68
s4ndhyac <sandhya2392@gmail.com>
2019-06-01 16:32:39 -0700

    thread functions and changes to existing syscalls wait

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -290,7 +290,7 @@ int wait(void)
     havekids = 0;
     for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
     {
-      if (p->parent != curproc)
+      if (p->isthread != 0 || p->parent != curproc)
         continue;
       havekids = 1;
       if (p->state == ZOMBIE)
@@ -541,4 +541,153 @@ void procdump(void)
     }
     cprintf("\n");
   }
+}
+
+int thread_create(void (*fcn)(void *), void *arg, void *stack)
+{
+  int i, pid;
+  struct proc *np;
+  struct proc *curproc = myproc();
+
+  // Allocate process -> allocate kernel stack page and find a UNUSED process, set it to EMBRYO state
+  if ((np = allocproc()) == 0)
+  {
+    return -1;
+  }
+
+  // Share address space instead of copying
+  np->pgdir = curproc->pgdir;
+
+  // copy file descriptors in same manner as fork
+  for (i = 0; i < NOFILE; i++)
+    if (curproc->ofile[i])
+      np->ofile[i] = filedup(curproc->ofile[i]);
+
+  // Copy process state from proc.
+  np->sz = curproc->sz;
+  np->parent = curproc;
+  np->cwd = idup(curproc->cwd);
+  safestrcpy(np->name, curproc->name, sizeof(curproc->name));
+
+  *np->tf = *curproc->tf;
+  // Clear %eax so that thread_create returns 0 in the child.
+  np->tf->eax = 0;
+  // new thread execution starts at fcn
+  np->tf->eip = (int)fcn;
+  // use stack as user stack
+  // esp points to the top of the stack
+  // stack is one page in size
+  np->tf->esp = (int)stack + PGSIZE;
+  // pass arg onto stack as argument
+  np->tf->esp -= 4;
+  *((int *)(np->tf->esp)) = (int)arg;
+  // use fake return PC -> 0xffffffff
+  np->tf->esp -= 4;
+  *((int *)(np->tf->esp)) = 0xffffffff;
+
+  // set isthread non-zero
+  np->isthread = 1;
+
+  // return PID of new thread to parent
+  pid = np->pid;
+  acquire(&ptable.lock);
+  np->state = RUNNABLE;
+  release(&ptable.lock);
+  return pid;
+}
+
+int thread_join(void)
+{
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+
+  acquire(&ptable.lock);
+  for (;;)
+  {
+    // Scan through table looking for exited threads
+    havekids = 0;
+    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    {
+      if (p->isthread == 0 || p->parent != curproc)
+        continue;
+      havekids = 1;
+      if (p->state == ZOMBIE)
+      {
+        // Found one.
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        // Not freeing shared address space of thread
+        //freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if (!havekids || curproc->killed)
+    {
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock); //DOC: wait-sleep
+  }
+}
+
+// Exit the current thread.  Does not return.
+// An exited thread remains in the zombie state
+// until its parent calls wait() to find out it exited.
+int thread_exit(void)
+{
+  struct proc *curproc = myproc();
+  struct proc *p;
+  int fd;
+
+  if (curproc == initproc)
+    panic("init exiting");
+
+  // Close all open files.
+  for (fd = 0; fd < NOFILE; fd++)
+  {
+    if (curproc->ofile[fd])
+    {
+      fileclose(curproc->ofile[fd]);
+      curproc->ofile[fd] = 0;
+    }
+  }
+
+  begin_op();
+  iput(curproc->cwd);
+  end_op();
+  curproc->cwd = 0;
+
+  acquire(&ptable.lock);
+
+  // Parent might be sleeping in wait().
+  wakeup1(curproc->parent);
+
+  // Pass abandoned children to init.
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+  {
+    if (p->parent == curproc)
+    {
+      p->parent = initproc;
+      if (p->state == ZOMBIE)
+        wakeup1(initproc);
+    }
+  }
+
+  // Jump into the scheduler, never to return.
+  curproc->state = ZOMBIE;
+  sched();
+  panic("zombie exit");
+  return 0;
 }
