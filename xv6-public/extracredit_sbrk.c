34d852d58c87411428d2ff7e792896ccdb947114
s4ndhyac <sandhya2392@gmail.com>
2019-06-08 14:30:23 -0700

    Revert "adding one more thread to test"
    This reverts commit f64b33e6a12970a100719896ce4b73da6cf7197a.

--- xv6-public/extracredit_sbrk.c
+++ xv6-public/extracredit_sbrk.c
@@ -56,35 +56,31 @@ int main(int argc, char *argv[])
   thread_mutex_init(&ml);
 
   struct balance b1 = {"b1", 50};
-  struct balance b2 = {"b2", 100};
-  struct balance b3 = {"b3", 150};
+  struct balance b2 = {"b2", 50};
+  struct balance b3 = {"b3", 100};
   struct balance b4 = {"b4", 200};
-  struct balance b5 = {"b5", 50};
 
-  void *s1, *s2, *s3, *s4, *s5;
-  int t1, t2, t3, t4, t5, r1, r2, r3, r4, r5;
+  void *s1, *s2, *s3, *s4;
+  int t1, t2, t3, t4, r1, r2, r3, r4;
 
   s1 = sbrk(4096);
   s2 = sbrk(4096);
   s3 = sbrk(4096);
   s4 = sbrk(4096);
-  s5 = sbrk(4096);
 
   t1 = thread_create(do_sbrk, (void *)&b1, s1);
-  t2 = thread_create(do_sbrk, (void *)&b2, s2);
-  t3 = thread_create(do_malloc, (void *)&b3, s3);
+  t2 = thread_create(do_malloc, (void *)&b2, s2);
+  t3 = thread_create(do_sbrk, (void *)&b3, s3);
   t4 = thread_create(do_malloc, (void *)&b4, s4);
-  t5 = thread_create(do_sbrk, (void *)&b5, s5);
 
   r1 = thread_join();
   r2 = thread_join();
   r3 = thread_join();
   r4 = thread_join();
-  r5 = thread_join();
 
   thread_mutex_lock(&ml);
   printf(1, "Threads finished: (%d):%d, (%d):%d, (%d):%d, (%d):%d\n",
-         t1, r1, t2, r2, t3, r3, t4, r4, t5, r5);
+         t1, r1, t2, r2, t3, r3, t4, r4);
   printf(1, "process size is %d after all child threads have exited\n", (int)sbrk(0));
   thread_mutex_unlock(&ml);
   exit();
