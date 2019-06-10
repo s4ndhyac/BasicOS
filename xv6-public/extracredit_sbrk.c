d75b6134877fa156ef88459f4374004d4a4c966b
s4ndhyac <sandhya2392@gmail.com>
2019-06-09 23:07:48 -0700

    removing malloc call in test prog

--- xv6-public/extracredit_sbrk.c
+++ xv6-public/extracredit_sbrk.c
@@ -29,24 +29,24 @@ void do_sbrk(void *arg)
   return;
 }
 
-void do_malloc(void *arg)
-{
+// void do_malloc(void *arg)
+// {
 
-  balance_t *b = (balance_t *)arg;
+//   balance_t *b = (balance_t *)arg;
 
-  thread_mutex_lock(&ml);
-  printf(1, "Thread %s malloc: process size is %d before doing malloc(%d)\n", b->name, (uint)sbrk(0), b->amount);
-  thread_mutex_unlock(&ml);
+//   thread_mutex_lock(&ml);
+//   printf(1, "Thread %s malloc: process size is %d before doing malloc(%d)\n", b->name, (uint)sbrk(0), b->amount);
+//   thread_mutex_unlock(&ml);
 
-  malloc(b->amount);
+//   malloc(b->amount);
 
-  thread_mutex_lock(&ml);
-  printf(1, "Thread %s malloc: after malloc(%d) process size = %d\n", b->name, b->amount, (uint)sbrk(0));
-  thread_mutex_unlock(&ml);
+//   thread_mutex_lock(&ml);
+//   printf(1, "Thread %s malloc: after malloc(%d) process size = %d\n", b->name, b->amount, (uint)sbrk(0));
+//   thread_mutex_unlock(&ml);
 
-  thread_exit();
-  return;
-}
+//   thread_exit();
+//   return;
+// }
 
 int main(int argc, char *argv[])
 {
@@ -55,30 +55,22 @@ int main(int argc, char *argv[])
 
   struct balance b1 = {"b1", 50};
   struct balance b2 = {"b2", 100};
-  struct balance b3 = {"b3", 150};
-  struct balance b4 = {"b4", 200};
 
-  void *s1, *s2, *s3, *s4;
-  int t1, t2, t3, t4, r1, r2, r3, r4;
+  void *s1, *s2;
+  int t1, t2, r1, r2;
 
   s1 = sbrk(4096);
   s2 = sbrk(4096);
-  s3 = sbrk(4096);
-  s4 = sbrk(4096);
 
   t1 = thread_create(do_sbrk, (void *)&b1, s1);
   t2 = thread_create(do_sbrk, (void *)&b2, s2);
-  t3 = thread_create(do_malloc, (void *)&b3, s3);
-  t4 = thread_create(do_sbrk, (void *)&b4, s4);
 
   r1 = thread_join();
   r2 = thread_join();
-  r3 = thread_join();
-  r4 = thread_join();
 
   thread_mutex_lock(&ml);
   printf(1, "Threads finished: (%d):%d, (%d):%d, (%d):%d, (%d):%d\n",
-         t1, r1, t2, r2, t3, r3, t4, r4);
+         t1, r1, t2, r2);
   printf(1, "process size is %d after all child threads have exited\n", (int)sbrk(0));
   thread_mutex_unlock(&ml);
   exit();
